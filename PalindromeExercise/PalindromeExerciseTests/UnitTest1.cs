45f5cda9a6010eec07b5c00430906e6b91a6b3f8
Justin Huth <Jayhuth96@gmail.com>
2023-02-27 14:39:32 -0600

    completed the excercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
-
+using PalindromeExercise;
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("hannah", true)]
+        [InlineData("racecar", true)]
+        [InlineData("radar", true)]
+        [InlineData("joy", false)]
+        [InlineData("ant", false)]
 
+        public static void IsAPalindromeTest(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = WordSmith.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
